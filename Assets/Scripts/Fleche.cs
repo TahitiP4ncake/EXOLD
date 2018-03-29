bea212576c68a5ac5b339a5ef18ed54b4cfc05c3
Dorian Beaugendre <dorian.pancake@gmail.com>
2018-03-29 18:01:43 +0200

    fleche ++

---
+++ Assets/Scripts/Fleche.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fleche : MonoBehaviour
+{
+
+	public Rigidbody rb;
+	
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.tag == "Wall")
+		{
+			Destroy(rb);
+		}
+	}
+}
