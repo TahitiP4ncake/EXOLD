2483eaa6c5029b640cf8e5b7459c880217ac9bbc
Dorian Beaugendre <dorian.pancake@gmail.com>
2018-03-30 16:41:13 +0200

    checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+	
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		FindObjectOfType<GameManager>().lastCheckpoint = transform.position;
+	}
+}
